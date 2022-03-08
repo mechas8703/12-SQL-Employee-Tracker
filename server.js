8f527831a0ba465e2c4991db005bf63f28e6f8fe
Maria Cardona <mechas8703@hotmail.com>
2022-03-08 18:59:21 -0500

    update package json

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const connnection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "Mamecago1941!",
     database: "employee_tracker"
 });
 
